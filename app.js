33a43b19b04577d20347b0eb3fb00223b4a74abd
Prithvi S. Bisht <prithvisbisht@gmail.com>
2018-05-06 05:01:05 +0530

    Routes file created nd messaging added

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // Connecting database
 mongoose.connect('mongodb://localhost/nodeapp');
@@ -38,6 +40,38 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
+// Express Session middleware
+app.use(session({
+  secret:'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+//Express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req,res,next) {
+  res.locals.messages =require('express-messages')(req,res);
+  next();
+});
+
+// express-validator Middleware
+app.use(expressValidator({
+  errorFormatter:function (param,msg,value) {
+    var namespace =param.split('.'),
+    root =namespace.shift(),
+    formParam=root;
+
+  while (namespace.length) {
+    formParam += '['+namespace.shift()+']';
+  }
+  return{
+    param:formParam,
+    msg:msg,
+    value :value
+  };
+}
+}))
+
 // Home Route
 app.get('/',function(req,res) {
   // res.send('Hello World!');
@@ -55,75 +89,10 @@ app.get('/',function(req,res) {
   });
 });
 
-//Get Single articles
-app.get('/article/:id',function (req , res) {
-  Article.findById(req.params.id, function (err,article) {
-    res.render('article',{
-      article:article
-    });
-  });
-});
+//Route Files
+let articles = require('./routes/routes');
+app.use('/articles',articles);
 
-app.get('/articles/add',function (req,res) {
-  res.render('add_article',{
-    title:'add article'
-  });
-});
-
-app.post('/articles/add',function(req,res) {
-  let article= new Article();
-  article.title =req.body.title;
-  article.author =req.body.author;
-  article.body =req.body.body;
-  article.save(function (err) {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-        res.redirect('/');
-    }
-  });
-});
-
-//Load EDit Form
-app.get('/article/edit/:id',function (req , res) {
-  Article.findById(req.params.id, function (err,article) {
-    res.render('edit_article',{
-      title:'Edit Article',
-      article:article
-    });
-  });
-});
-
-// Update Submit POST route
-app.post('/articles/edit/:id',function(req,res) {
-  let article= {};
-  article.title =req.body.title;
-  article.author =req.body.author;
-  article.body =req.body.body;
-
-  let query={_id:req.params.id}
-
-  Article.update(query, article, function (err) {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-        res.redirect('/');
-    }
-  });
-});
-
-app.delete('/article/:id',function (req,res) {
-  let query = {_id:req.params.id}
-
-  Article.remove(query, function(err){
-    if(err){
-      console.log(err);
-    }
-    res.send('Success');
-  });
-});
 // Start Server
 app.listen(3000,function () {
   console.log('Server Started on port 3000');
